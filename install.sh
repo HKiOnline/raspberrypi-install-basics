71813489a103d34025dcaecf02b0620734277ca9
Henri Kesseli <projects@hkionline.net>
2021-06-21 16:40:30 +0300

    Updated to include utilities installation

--- install.sh
+++ install.sh
@@ -16,8 +16,10 @@ fi
 
 
 echo "Updating repositories and upgrading packages"
-curl -fsSL https://raw.githubusercontent.com/HKiOnline/raspberrypi-install-basics/main/install_upgrades.sh | sh
+curl -fsSL https://raw.githubusercontent.com/HKiOnline/raspberrypi-install-basics/main/install_upgrades.sh | bash
 
+echo "Install essential utilities"
+curl -fsSL https://raw.githubusercontent.com/HKiOnline/raspberrypi-install-basics/main/install_utilities.sh" | bash
 
 echo "Installing Docker and Docker Compose"
-curl -fsSL https://raw.githubusercontent.com/HKiOnline/raspberrypi-install-basics/main/install_docker.sh | sh
+curl -fsSL https://raw.githubusercontent.com/HKiOnline/raspberrypi-install-basics/main/install_docker.sh | bash
